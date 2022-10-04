c7e8504e189e178ae2a30a9507e6fa56b1731d30
Sym Roe <sym.roe@talusdesign.co.uk>
2022-10-04 22:30:17 +0100

    Convert move API views to ViewSets
    This time we want to use ViewSets to get the advantage of the auto-API
    pages, but we don't want the detail pages. So we use the mixins from DRF

--- bears/views.py
+++ bears/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, mixins
 from bears.models import Bear, Result, BracketContestant
 from bears.serializers import BearSerializer, ContestantSerializer
 
@@ -10,15 +10,11 @@ class BearViewSet(viewsets.ModelViewSet):
     serializer_class = BearSerializer
 
 
-class ChampionsView(APIView):
-    def get(self, request, *args, **kwargs):
-        champions = BracketContestant.objects.champions()
-        serializer = ContestantSerializer(champions, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class ChampionsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = BracketContestant.objects.champions()
+    serializer_class = ContestantSerializer
 
 
-class FinalistsView(APIView):
-    def get(self, request, *args, **kwargs):
-        finalists = BracketContestant.objects.finalists()
-        serializer = ContestantSerializer(finalists, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class FinalistsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = BracketContestant.objects.finalists()
+    serializer_class = ContestantSerializer
--- fat_bear_week_api/urls.py
+++ fat_bear_week_api/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from bears.views import ChampionsView, FinalistsView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("", include("api.urls")),
-    path("api/champions", ChampionsView.as_view()),
-    path("api/finalists", FinalistsView.as_view()),
 ]
