008a168e7705eb7367c1fd79228d457532ff9800
Sym Roe <sym.roe@talusdesign.co.uk>
2022-10-04 23:18:51 +0100

    Add an admin interface for bears
    See this by creating an admin user (`python manage.py createsuperuser`)
    and logging in via `/admin/`

--- bears/admin.py
+++ bears/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from bears.models import Bear
+
+
+class BearAdmin(admin.ModelAdmin):
+    search_fields = ("bear_name",)
+    list_display = ["bear_name", "bear_number"]
+    list_filter = ["bear_gender", "bear_size"]
+    readonly_fields = ("bear_uuid",)
+
+
+admin.site.register(Bear, BearAdmin)
